d116619b3c403f15faa283002b0c76af56590e69
Suraj Kumar <surajkumarok20@gmail.com>
2024-06-24 23:26:30 +0530

    Day1

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema =  new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        set: (v) => v === "" ? "https://www.istockphoto.com/photo/old-ways-wont-open-new-doors-gm696063644-129710361?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Ffree-images&utm_medium=affiliate&utm_source=unsplash&utm_term=free+images%3A%3A%3A" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.export = Listing;
